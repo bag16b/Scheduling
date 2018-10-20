16b28e06e3b33da76064a78099b59da390eb88c6
bag16b <bag16b@acu.edu>
2018-10-20 17:12:50 -0500

    More bracket plox

--- kernel/clock.c
+++ kernel/clock.c
@@ -184,9 +184,11 @@ PUBLIC int ap_timer_int_handler(void)
 
 	timeSecond -= ticks;
 	if(timeSecond <= 0) {
-       for(q = 0; q < NR_SCHED_QUEUES; q++)
-			for(p = rdy_head[q]; p; p = p->p_nextready)
+       for(q = 0; q < NR_SCHED_QUEUES; q++) {
+		for(p = rdy_head[q]; p; p = p->p_nextready) {
 				p->p_user_time = 0;
+			}
+	   }		
 		timeSecond = 5;
 	}
 	

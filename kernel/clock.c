3008dfa2e61e514c2e5aaaa89f6582d41f6f04df
bag16b <bag16b@acu.edu>
2018-10-22 13:22:20 -0500

    RIP OS

--- kernel/clock.c
+++ kernel/clock.c
@@ -183,8 +183,9 @@ PUBLIC int ap_timer_int_handler(void)
 	struct proc * p, * billp;
 
 	timeSecond -= ticks;
+	printf("%d", timeSecond);
 	if(timeSecond <= 0) {
-	   printf("%d", timeSecond);
+	   
 	   timeSecond = 200;
        for(q = 0; q < NR_SCHED_QUEUES; q++) {
 		for(p = rdy_head[q]; p; p = p->p_nextready) {

0ba361a54672ce2c0c27564143c5f2be8cd243d3
bag16b <bag16b@acu.edu>
2018-10-22 13:19:31 -0500

    Added more prints

--- kernel/clock.c
+++ kernel/clock.c
@@ -184,6 +184,7 @@ PUBLIC int ap_timer_int_handler(void)
 
 	timeSecond -= ticks;
 	if(timeSecond <= 0) {
+	   printf("%d", timeSecond);
 	   timeSecond = 200;
        for(q = 0; q < NR_SCHED_QUEUES; q++) {
 		for(p = rdy_head[q]; p; p = p->p_nextready) {
